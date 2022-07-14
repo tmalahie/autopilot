95d22c2ec7e1b175e5b19289d56aed9586ee0b2f
tmalahie <t.malahieude@gmail.com>
2022-07-14 12:20:32 +0200

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,52 @@
+import * as vscode from 'vscode';
+
+// Try it out in `playground.js`
+
+export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand(
+		'extension.autopilot-settings',
+		() => {
+			vscode.window.showInformationMessage('Show settings');
+		}
+	);
+
+	context.subscriptions.push(disposable);
+	let someTrackingIdCounter = 0;
+
+	const provider: vscode.InlineCompletionItemProvider = {
+		provideInlineCompletionItems: async (document, position, context, token) => {
+			console.log('provideInlineCompletionItems triggered');
+
+			const regexp = /\/\/ \[(.+),(.+)\):(.*)/;
+			if (position.line <= 0) {
+				return;
+			}
+
+			const lineBefore = document.lineAt(position.line - 1).text;
+			const matches = lineBefore.match(regexp);
+			if (matches) {
+				const start = matches[1];
+				const startInt = parseInt(start, 10);
+				const end = matches[2];
+				const endInt =
+					end === '*' ? document.lineAt(position.line).text.length : parseInt(end, 10);
+				const insertText = matches[3].replace(/\\n/g, '\n');
+
+				return [
+					{
+						insertText,
+						range: new vscode.Range(position.line, startInt, position.line, endInt),
+						someTrackingId: someTrackingIdCounter++,
+					},
+				] as MyInlineCompletionItem[];
+			}
+		},
+	};
+
+	vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, provider);
+
+}
+
+interface MyInlineCompletionItem extends vscode.InlineCompletionItem {
+	someTrackingId: number;
+}
